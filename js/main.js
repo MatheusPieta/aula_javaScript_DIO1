e1e78aea4907188ed137466fc327687993ae6457
MatheusPieta <matheus_pieta@hotmail.com>
2022-12-13 22:11:58 -0300

    CursoBasico

---
+++ js/main.js
@@ -0,0 +1,98 @@
+function clicou(){
+    document.getElementById("agradecimento").innerHTML + "<b>Obrigado por clicar</b>";
+    //alert("Obrigado por clicar!");
+};
+
+function redirecionar(){
+    window.open("https://github.com/MatheusPieta");
+};
+
+/*
+function soma (n1, n2){
+    return n1 + n2;
+}
+
+function setReplace(frase, nome, novo_nome){
+   return frase.replace(nome, novo_nome);
+}
+
+alert(soma(5,10));
+alert(setReplace("Vai Japão", "Japão", "Brasil"));
+*/
+
+/*
+function validadeIdade(idade){
+    var validar;
+    if (idade >= 18){
+        validar = true
+    }else{
+        validar = false;
+    }
+    return validar;
+}
+
+var idade = prompt("Qual sua idade");
+console.log(validadeIdade(idade));
+*/
+
+/*
+var d = new Date();
+alert(d.getDay());
+*/
+
+/*
+var count;
+for(count=0; count <= 5; count++){
+    alert(count);
+}
+*/
+
+/*
+var count = 0;
+while (count <= 5) {
+    console.log(count);
+    alert(count);
+    count++;
+}
+*/
+
+/*
+var idade = prompt("Qual sua idade?");
+if(idade > 18){
+    alert("Maior de idade");
+}else{
+    alert("Menor de idade");
+};
+*/
+
+/*
+var frutas = [{nome:"Maça", cor:"Vermelha"}, {nome:"Uva", cor:"Roxa"}];
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+
+/*
+var fruta = {nome:"Maça", cor:"Vermelha"};
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//var lista = ["Maça", "Pera", "Laranja"];
+//lista.push("Uva");
+//lista.pop();
+//console.log(lista[1]);
+//alert(lista[1]);
+
+//console.log(lista.reverse());
+//console.log(lista.toString());
+//console.logo(lista.join(" -"));
+
+//var nome = "Matheus Pieta da Cunhna";
+//var idade = 21;
+//var idade2 = 10;
+//var frase = "A SELEÇÃO Brasileira é ruim "
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(frase.replace("Brasileira", "Belga"));
+//console.log(frase.toUpperCase());
